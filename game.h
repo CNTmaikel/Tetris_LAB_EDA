09f2d75cc026e421d3e15cdff2bf4100316f9cd7
Miquel <54858840+CNTmaikel@users.noreply.github.com>
2025-02-06 18:01:18 +0100

    Update game.h

--- game.h
+++ game.h
@@ -11,35 +11,25 @@
 
 typedef struct{
     // ToDo in LAB 1 - define data structure FET!!
-    Piece p;
-    int at_row;
-    int at_col;
+    Piece p; // La peça actual que s'està movent
+    int at_row; // Posició actual de la peça en files
+    int at_col; // Posició actual de la peça en columnes
 
 } PieceInfo;
 
 typedef struct{
     // ToDo in LAB 1 - define data structure FET!!!
-    int score;
-    char board[MAX_ROWS][MAX_COLUMNS];
-    PieceInfo current_piece;
+    int score; // Puntuació del jugador
+    char board[MAX_ROWS][MAX_COLUMNS]; // Matriu que representa el tauler de joc
+    PieceInfo current_piece; // Informació de la peça en moviment
 
 } GameState;
 
-/// Implemented functions
-void print_game_state(GameState *gs);
-void print_options();
-bool is_valid_option(int option);
-void print_line();
-void print_board(GameState *game_state);
-void get_new_random_piece(GameState *game_state);
-void block_current_piece(GameState *game_state);
-bool is_collision(char board[MAX_ROWS][MAX_COLUMNS], PieceInfo *piece_info);
-
 /**** LAB 1 - functions to program (start here) ****/
-void init_game_state(GameState *game_state);
-bool is_terminal(char board[MAX_ROWS][MAX_COLUMNS]); // True if contains X values in TOP-4 rows
-void move(char board[MAX_ROWS][MAX_COLUMNS], PieceInfo *piece_info, int option);
-void rotate(char board[MAX_ROWS][MAX_COLUMNS], PieceInfo *piece_info, int option);
+void init_game_state(GameState *game_state); // Inicialitza el joc amb una puntuació de 0 i el tauler buit
+bool is_terminal(char board[MAX_ROWS][MAX_COLUMNS]); // Comprova si el joc ha acabat (una peça bloquejada en les 4 files superiors)
+void move(char board[MAX_ROWS][MAX_COLUMNS], PieceInfo *piece_info, int option); // Mou la peça a l'esquerra o a la dreta
+void rotate(char board[MAX_ROWS][MAX_COLUMNS], PieceInfo *piece_info, int option); // Gira la peça en el sentit indicat
 /**** LAB 1 - functions to program (end here) ****/
 
 /// Implemented functions
@@ -47,4 +37,3 @@ void run_turn(GameState *game_state, int option);
 
 
 #endif
-
